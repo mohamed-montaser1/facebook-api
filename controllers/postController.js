3315d6c47a18424a99a1c0eac1069a41f1a21d15
mohamed montaser <mastrocoding@gmail.com>
2025-04-17 11:20:55 +0200

    finish update post

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,9 +25,42 @@ exports.createPost = async (req, res) => {
   }
 };
 
-exports.updatePost = (req, res) => {};
+exports.updatePost = async (req, res) => {
+  try {
+    const newPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(200).json({
+      status: "success",
+      data: { post: newPost },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "fail",
+      message: error.message,
+      error,
+    });
+  }
+};
 
 exports.deletePost = (req, res) => {};
 
 // Check if the user have permissions to manipulate posts or create ones
-exports.checkPermissions = (req, res) => {};
+exports.checkPermissions = async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    return res.status(404).json({ status: "fail", message: "Post not found" });
+  }
+  const author = post.author;
+  const user = req.user;
+
+  if (user.id !== author) {
+    return res.status(403).json({ status: "fail", message: "forbidden!" });
+  }
+
+  return next();
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const controllers = require("../controllers/postController");
 const userControllers = require("../controllers/userController");
 
-router.post("/create", userControllers.checkUserLogin, controllers.createPost);
+router.post("/", userControllers.checkUserLogin, controllers.createPost);
+router.patch(
+  "/:id",
+  userControllers.checkUserLogin,
+  controllers.checkPermissions,
+  controllers.updatePost
+);
 
 module.exports = router;
